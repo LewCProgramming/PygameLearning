7fe3a4fd17992a67ec8c3ade040c3de6690c7603
captainmiau <89042360+captainmiau@users.noreply.github.com>
2023-06-27 18:27:28 -0700

    Update PygameLearning.py

--- PygameLearning.py
+++ PygameLearning.py
@@ -6,9 +6,8 @@ sourceFileDir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(sourceFileDir)
 
 def display_score():
-    current_time = pygame.time.get_ticks()
-    current_time = round(current_time/1000)
-    score_surface = test_font.render(f'{current_time}', False, (64,64,64))
+    current_time = int(pygame.time.get_ticks()/1000) - start_time
+    score_surface = test_font.render(f'Score: {current_time}', False, (64,64,64))
     score_rectangle = score_surface.get_rect(center = (400,50))
     screen.blit(score_surface,score_rectangle)
 
@@ -17,7 +16,8 @@ screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('LewGame')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font(None, 50)
-game_active = True
+game_active = False
+start_time = 0
 
 sky_surface = pygame.image.load('./sky.png').convert()
 ground_surface = pygame.image.load('./ground.png').convert()
@@ -35,6 +35,18 @@ player_surface = pygame.image.load('./Graphics/Player/p1_walk/PNG/p1_walk01.png'
 player_rectangle = player_surface.get_rect(midbottom = (80,305))
 player_gravity = 0
 
+# Intro Screen
+player_stand = pygame.image.load('./Graphics/Player/p1_front.png').convert_alpha()
+player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
+player_stand_rectangle = player_stand.get_rect(center = (400, 200))
+
+game_name = test_font.render('LewGame', False, (85, 196, 100))
+game_name_rectangle = game_name.get_rect(center = (400, 80))
+
+game_message = test_font.render('Press space to run.', False, ('red'))
+game_message_rectangle = game_message.get_rect(center = (400, 320))
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -44,14 +56,16 @@ while True:
             if player_rectangle.collidepoint(event.pos):
                 player_gravity = -20"""
 
-        if event.type == pygame.KEYDOWN and player_rectangle.bottom >= 305:
+        if event.type == pygame.KEYDOWN and player_rectangle.bottom >= 305 and game_active == True:
             if event.key == pygame.K_SPACE:
                 player_gravity = -20
         else:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and game_active == False:
+                print('button')
                 game_active = True
                 snail_rectangle.left = 800
-    
+                start_time = int(pygame.time.get_ticks()/1000) 
+     
     screen.blit(scaled_sky_surface,(0,0))
     screen.blit(scaled_ground_surface,(0,300))
     #pygame.draw.rect(screen,'#c0e8ec',score_rectangle)
@@ -80,7 +94,10 @@ while True:
         if snail_rectangle.colliderect(player_rectangle):
             game_active = False
     else:
-        screen.fill('Yellow')
+        screen.fill((50,100,160))
+        screen.blit(player_stand, player_stand_rectangle)
+        screen.blit(game_name, game_name_rectangle)
+        screen.blit(game_message, game_message_rectangle)
 
     pygame.display.update()
     clock.tick(60)
